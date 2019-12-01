cc75dbabab78e9f5c25667e24f2f987609b51709
yang zhang <248122551@qq.com>
2019-12-01 21:32:53 +0800

    移动端-首页-模拟不同的频道加载不同文章列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,8 +7,14 @@
       <van-tab :title="item.name" v-for="item in channels" :key="item.id">
         <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
           <!-- 内容列表 -->
-          <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-            <van-cell v-for="item in list" :key="item" :title="item" />
+          <!-- v-model="loading" 控制上拉加载更多的 loading加载中
+          :finished="finished" 控制是否加载结束
+             true 不在触发加载更多了，直接显示“没有跟多了”  false 继续触发加载跟多
+          finished-text="没有更多了"  设置没有数据之后的提示文本
+          @load="onLoad" 上拉加载更多触发的自定义事件-->
+          <van-list v-model="loading" :finished="item.finished" finished-text="没有更多了" @load="onLoad">
+            <p>{{item.name }}</p>
+            <van-cell v-for="item in item.articles" :key="item" :title="item" />
           </van-list>
         </van-pull-refresh>
       </van-tab>
@@ -25,7 +31,7 @@ export default {
   data () {
     return {
       channels: [], // 频道列表
-      active: '',
+      active: '', // 频道列表索引
       list: [],
       loading: false,
       finished: false,
@@ -38,20 +44,27 @@ export default {
     this.loadgetUserChannels()
   },
   methods: {
+    // 上拉加载更多触发的自定义事件
     onLoad () {
-      // 异步更新数据
+      // 获取当前频道
+      const activeChannel = this.channels[this.active]
+      // 当前频道的文章列表
+      const articles = activeChannel.articles
+      // 1. 请求加载数据
       setTimeout(() => {
+        // 2. 将数据添加到当前频道的文章列表中
         for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
+          articles.push(articles.length + 1)
         }
+        // 3. 将 loading 设置为 false
         // 加载状态结束
+        // 关闭本次的 loading 状态，它会判定当前数据是否够一屏，不够就继续自动 onLoad
         this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
+        // 4. 判断如果没有数据了，将当前频道的结束状态 finished 设置为 true
+        if (articles.length >= 40) {
+          activeChannel.finished = true
         }
-      }, 500)
+      }, 1000)
     },
 
     onRefresh () {
@@ -66,7 +79,14 @@ export default {
     async loadgetUserChannels () {
       const res = await getUserChannels()
       // console.log(res)
-      this.channels = res.data.data.channels
+      const channels = res.data.data.channels
+      // 给每一个频道添加一个数据 用来存储频道的文章列表
+      channels.forEach(element => {
+        element.articles = [] // 频道的文章列表
+        element.finished = false // 频道的加载结束状态
+      })
+      // console.log(channels)
+      this.channels = channels
     }
   }
 }
--- src/views/tabbar/index.vue
+++ src/views/tabbar/index.vue
@@ -3,10 +3,10 @@
     <!-- 二级路由组件 -->
     <router-view></router-view>
     <van-tabbar v-model="active">
-      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="search">标签</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">查询</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
